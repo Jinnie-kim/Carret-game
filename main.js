22df7b52029f5dde6612ae2f08a0611d5be32db5
HeeJin <hjk7639@gmail.com>
2022-01-25 15:21:06 +0900

    JS: 게임 시작하기 수정
    1. 게임의 상태를 기억하는 변수 설정
    2. 게임 시작, 멈춤 함수 생성
    3. 게임 시작 함수에 게임 필드 만들기, 멈춤버튼&타이머&스코어 보이기 함수 넣기, 타이머 시작 함수 넣기
    4. 멈춤버튼 보여주는 함수 생성
    5. 타이머&스코어 보여주는 함수 생성
    +CSS 해당 요소에 visibility 속성 추가

--- main.js
+++ main.js
@@ -1,10 +1,56 @@
 'use strict';
 const CARROT_SIZE = 80;
+const CARROT_COUNT = 5;
+const BUG_COUNT = 5;
+
 const field = document.querySelector('.game__field');
 const fieldRect = field.getBoundingClientRect();
-const playBtn = document.querySelector('.game__button');
-const popUp = document.querySelector('.pop-up');
-const score = document.querySelector('.game__score');
+const gameBtn = document.querySelector('.game__button');
+const gameTimer = document.querySelector('.game__timer');
+const gameScore = document.querySelector('.game__score');
+
+// 게임의 상태를 기억하고 있는 변수가 필요
+
+let started = false;
+let score = 0; 
+let timer = undefined;
+
+gameBtn.addEventListener('click', () => {
+    if(started) {
+        stopGame();
+    } else {
+        startGame();
+    }
+    started = !started; // 버튼 클릭할때마다 게임의 상태를 바꿔주는 것
+})
+
+
+function startGame() {
+    initGame();
+    showStopButton();
+    showTimerandScore();
+    startGameTimer();
+}
+
+function stopGame() {
+
+}
+
+function showStopButton() {
+    const icon = document.querySelector('.fa-play');
+    icon.classList.add('fa-stop');
+    icon.classList.remove('fa-play');
+}
+
+function showTimerandScore() {
+    gameTimer.style.visibility = 'visible';
+    gameScore.style.visibility = 'visible';
+}
+
+
+
+
+
 
 function initGame() {
     // (거의) 생성된 벌레와 당근을 필드에 올린다.
@@ -18,7 +64,7 @@ function addItem(className, count, imgPath) {
     const y1 = 0;
     const x2 = fieldRect.width - CARROT_SIZE;
     const y2 = fieldRect.height - CARROT_SIZE;
-    // for문의 목적..? i의 최대 개수 까지 반복하면서 각 요소를 만든다..홀리쓋..
+    // for문의 목적..? i의 최대 개수 까지 반복하면서 각 요소를 만들고 동시에 랜덤좌표 할당..홀리쓋..
     for (let i = 0; i < count; i++) {
         const item = document.createElement('img');
         item.setAttribute('class', className);
@@ -36,12 +82,3 @@ function randomNumber(min, max) {
     return Math.random() * (max - min) + min;        
 }
 
-playBtn.addEventListener('click', () => {
-    initGame();
-    setTimeout(message, 5000);
-    score.innerHTML = '5';
-})
-
-function message() {
-    popUp.classList.remove('pop-up--hide');
-}
